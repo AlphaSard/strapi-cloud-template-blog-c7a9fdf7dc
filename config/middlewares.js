b599e2be1d86019cca2bc7ca24506c747d60de50
Erwan Wahad Soumhi <erwan.soumhi@gmail.com>
2025-08-26 02:43:57 +0200

    CORS middlewares config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	"strapi::errors",
+	{
+		name: "strapi::cors",
+		config: {
+			origin: [
+				"https://<ton-site>.vercel.app", // prod
+				"https://<ton-site>-git-*.vercel.app", // previews (pattern)
+				"http://localhost:3000", // dev front
+			],
+			methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+			headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+			keepHeaderOnError: true,
+			credentials: true,
+		},
+	},
+	"strapi::security",
+	"strapi::logger",
+	"strapi::query",
+	"strapi::body",
+	"strapi::session",
+	"strapi::favicon",
+	"strapi::public",
 ];
