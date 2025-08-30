2bb11dcd198e198ed008138871818609c9f8c6ff
Erwan Wahad Soumhi <erwan.soumhi@gmail.com>
2025-08-30 18:02:56 +0200

    Configure import-export plugin; cleanup middlewares; prepare for cloud deploy

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'strapi-import-export': {
+    config: {
+      // Utilisé par le plugin pour construire les URL d'export
+      serverPublicHostname: env('SERVER_PUBLIC_HOSTNAME', 'http://localhost:1337'),
+    },
+  },
+});
